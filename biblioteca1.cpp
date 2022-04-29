87de977daa165b25b9bad24d0b4483e201048269
DarwingJuarez <100969487+DarwingJuarez@users.noreply.github.com>
2022-04-28 19:36:56 -0600

    Add files via upload

---
+++ biblioteca1.cpp
@@ -0,0 +1,6 @@
+#include"biblioteca1.h"
+
+int suma(int a, int b){
+ return a+b;
+
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"biblioteca1.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+
+
+ int resultado = suma(9,27);
+ cout << "El resultado es: " << resultado << "\n";
+ return 0;
+
+	return 0;
+}
